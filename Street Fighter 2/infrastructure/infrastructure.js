d204687a48ea6d154e0dfeae2fce009e5a611ea0
Miguelz <62753251+Migueleo198@users.noreply.github.com>
2025-02-03 07:51:39 +0100

    Update infrastructure.js

--- Street Fighter 2/infrastructure/infrastructure.js
+++ Street Fighter 2/infrastructure/infrastructure.js
@@ -26,7 +26,7 @@ window.onload = function() {
 document.addEventListener('click',loadAudio);
 
 function loadAudio(){
-  audio.volume = Math.min(audio.volume -0.5, 1.0); // Increase by 10%, max 1.0
+  
   audio.load();
   audio.play();
   audio.loop=true;
