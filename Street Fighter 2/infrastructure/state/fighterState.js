fd13532e575ece42cc9fac4777ceaa43af8a1a0c
Miguelz <62753251+Migueleo198@users.noreply.github.com>
2025-02-06 07:52:26 +0100

    Delete Street Fighter 2 directory

--- Street Fighter 2/infrastructure/state/fighterState.js
+++
@@ -1,9 +0,0 @@
-import { HEALTH_MAX_HIT_POINTS } from "../../application/battle.JS";
-
-export const createDefaultFighterState = (id) => ({
-    id,
-    score:1,
-    battles:0,
-    hitPoints: HEALTH_MAX_HIT_POINTS,
-
-})
